6133c7335961e1924447b9f4382732b953abe585
iznot <anais.glur@gmail.com>
2022-07-24 10:55:08 +0200

    requirements

---
+++ basis_game/basic_game.py
@@ -0,0 +1,16 @@
+i = 1
+print(type(i))
+while i < 6:
+    print(i)
+    i += 1
+
+i = "hoi anais"
+print(type(i))
+
+i = True
+print(type(i))
+
+
+spielfeld = [True, False, False, True]
+print(spielfeld)
+print(type(spielfeld))
