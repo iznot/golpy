ac734d28cc5e51d16653ffc96bc30d9b1e000839
iznot <anais.glur@gmail.com>
2022-07-25 15:14:28 +0200

    Jupyter

--- basis_game/basic_game.py
+++ basis_game/basic_game.py
@@ -1,3 +1,9 @@
+from lib2to3.pgen2.pgen import DFAState
+from types import AsyncGeneratorType
+from typing_extensions import assert_type
+import numpy as np
+from pandas import DataFrame
+
 i = 1
 print(type(i))
 while i < 6:
@@ -14,3 +20,20 @@ print(type(i))
 spielfeld = [True, False, False, True]
 print(spielfeld)
 print(type(spielfeld))
+
+a = np.zeros(8)
+print(a)
+
+a = np.array(4, dtype= bool)
+print(a)
+
+a = np.zeros(4, dtype = bool)
+print(a)
+
+matrix = [[False for i in range(4)] for j in range(4)]
+print(matrix)
+
+def shw():
+    print("hello")
+    r = divide(2, 5) 
+    print(r) 
