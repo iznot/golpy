a74513d427aba6907d19c18f308dfa2c7843b7f3
Christoph Glur <christoph.glur@powerpartners.pro>
2022-10-02 16:13:04 +0200

    gui

--- basis_game/gui.py
+++ basis_game/gui.py
@@ -1,46 +1,47 @@
 from time import sleep
-import numpy as np
+
 import basic_game_functions as gamefun
-import os 
+import samples
+
 import tkinter as tk 
 
 
-gameboard = gamefun.create_gameboard(rows = 9, cols = 10)
+class GolpyGui():
 
-def gui():
-    window = tk.Tk()
-    gameboard_label = tk.Label(text = gamefun.get_gameboard_text(gameboard, separator = False))
-    gameboard_label.configure(font=("Consolas", 12))
-    gameboard_label.pack()
+    def __init__(self, gameboard):
+
+        window = tk.Tk()
+        self.gameboard = gameboard
+        self.gameboard_label = tk.Label(text = gamefun.get_gameboard_text(self.gameboard, horizontal_separator = False))
+        self.gameboard_label.configure(font=("Webdings", 12))
+        self.gameboard_label.pack()
     
 
-    button = tk.Button(
-        text = "next",
-        width=15,
-        height = 3,
-        bg="blue",
-        fg = "yellow",
-        command = lambda: nextCallBack(gameboard_label)
-    )
+        button = tk.Button(
+            text = "next",
+            width=15,
+            height = 3,
+            bg="blue",
+            fg = "yellow",
+            command = self.nextCallBack
+        )
     
-    button.pack()
+        button.pack()
     
-    tk.mainloop()
+        tk.mainloop()
     
     
-def nextCallBack(gameboard_label):
-    gameboard2 = gamefun.play(gameboard)
-    gameboard_label["text"] = gamefun.get_gameboard_text(gameboard2, separator=False)
-    gameboard_label.pack()
-    gameboard = gameboard2
+    def nextCallBack(self):
+        
+        self.gameboard = gamefun.play(self.gameboard)
+        self.gameboard_label["text"] = gamefun.get_gameboard_text(self.gameboard, horizontal_separator=False)
+        self.gameboard_label.pack()
+        
 
 
 def main():
-    
-    gameboard[3,4] = True
-    gameboard[4,4] = True
-    gameboard[5,4] = True
-    gui()
+    gleiter = samples.get_gleiter(16)
+    GolpyGui(gleiter)
 
 if __name__ == "__main__":
     main()
