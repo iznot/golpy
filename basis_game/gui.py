6406fc91dce541c827832664827f200b67c2b458
Christoph Glur <christoph.glur@powerpartners.pro>
2022-10-02 15:44:23 +0200

    next

--- basis_game/gui.py
+++ basis_game/gui.py
@@ -1,25 +1,46 @@
+from time import sleep
 import numpy as np
 import basic_game_functions as gamefun
 import os 
 import tkinter as tk 
 
 
-def gui(gameboard : np.array):
-    window = tk.Tk()
-    gameboard_gui = tk.Label(text = gamefun.get_gameboard_text(gameboard, separator = False))
-    gameboard_gui.configure(font=("Consolas", 12))
-    gameboard_gui.pack()
-    window.mainloop()
-    print("End")
+gameboard = gamefun.create_gameboard(rows = 9, cols = 10)
 
+def gui():
+    window = tk.Tk()
+    gameboard_label = tk.Label(text = gamefun.get_gameboard_text(gameboard, separator = False))
+    gameboard_label.configure(font=("Consolas", 12))
+    gameboard_label.pack()
+    
+
+    button = tk.Button(
+        text = "next",
+        width=15,
+        height = 3,
+        bg="blue",
+        fg = "yellow",
+        command = lambda: nextCallBack(gameboard_label)
+    )
+    
+    button.pack()
+    
+    tk.mainloop()
+    
+    
+def nextCallBack(gameboard_label):
+    gameboard2 = gamefun.play(gameboard)
+    gameboard_label["text"] = gamefun.get_gameboard_text(gameboard2, separator=False)
+    gameboard_label.pack()
+    gameboard = gameboard2
 
 
 def main():
-    g0 = gamefun.create_gameboard(rows = 9, cols = 10)
-    g0[3,4] = True
-    g0[4,4] = True
-    g0[5,4] = True
-    gui(g0)
+    
+    gameboard[3,4] = True
+    gameboard[4,4] = True
+    gameboard[5,4] = True
+    gui()
 
 if __name__ == "__main__":
     main()
