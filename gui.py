61000814654d0dac06812a0b7eb673f753260da7
Christoph Glur <christoph.glur@powerpartners.pro>
2022-12-10 17:06:14 +0100

    gui paint

--- gui.py
+++ gui.py
@@ -4,10 +4,78 @@ import basic_game_functions as gm
 import gameboard_manipulation as gam
 import run_simulation as sim
 import samples
-
+import numpy as np
 import tkinter as tk 
 
 
+class DrawableGrid(tk.Frame):
+    def __init__(self, parent, golpy, gameboard, size=5):
+        super().__init__(parent, bd=1, relief="sunken")
+        self.golpy = golpy
+        self.height = gameboard.shape[0]
+        self.width = gameboard.shape[1]
+        self.size = size
+        canvas_width = self.width*size
+        canvas_height = self.height*size
+        self.canvas = tk.Canvas(self, bd=0, highlightthickness=0, width=canvas_width, height=canvas_height)
+        self.canvas.pack(fill="both", expand=True, padx=2, pady=2)
+
+        for row in range(self.height):
+            for column in range(self.width):
+                x0, y0 = (column * self.size), (row*self.size)
+                x1, y1 = (x0 + self.size), (y0 + self.size)
+                color = "black" if gameboard[row, column] == True else "white"
+                self.canvas.create_rectangle(x0, y0, x1, y1,
+                                             fill=color, outline="gray",
+                                             tags=(self._tag(row, column),"cell" ))
+
+        self.canvas.tag_bind("cell", "<B1-Motion>", self.paint)
+        self.canvas.tag_bind("cell", "<1>", self.paint)
+
+    def _tag(self, row, column):
+        """Return the tag for a given row and column"""
+        tag = f"{row},{column}"
+        return tag
+
+    def get_pixels(self):
+        row = ""
+        for row in range(self.height):
+            output = ""
+            for column in range(self.width):
+                color = self.canvas.itemcget(self._tag(row, column), "fill")
+                value = "1" if color == "black" else "0"
+                output += value
+        return output
+    
+    def set_gameboard(self, gameboard):
+        for row in range(self.height):
+            for column in range(self.width):
+                x0, y0 = (column * self.size), (row*self.size)
+                #x1, y1 = (x0 + self.size), (y0 + self.size)
+                color = "black" if gameboard[row, column] == True else "white"
+                cell = self.canvas.find_closest(x0, y0)
+                self.canvas.itemconfigure(cell, fill = color)
+
+
+
+    def get_gameboard(self):
+        gb = np.full((self.height, self.width), False)
+        for row in range(self.height):
+            for column in range(self.width):
+                color = self.canvas.itemcget(self._tag(row, column), "fill")
+                if color == "black":
+                    gb[row, column] = True
+        return gb
+        
+
+    def paint(self, event):
+        cell = self.canvas.find_closest(event.x, event.y)
+        color = self.canvas.itemcget(cell, "fill")
+        color = "black" if color == "white" else "white"
+        self.canvas.itemconfigure(cell, fill=color)
+        self.golpy.set_text_to_value("")
+        self.golpy.gameboard = self.get_gameboard()
+        
 
 class GolpyGui():
 
@@ -36,19 +104,30 @@ class GolpyGui():
         sample.set(sample_key)
         self.sample_selector = tk.OptionMenu(self.text_frame, sample, command = self.selectSample, *options)
 
-        self.accept_button = tk.Button(
+        self.paint_button = tk.Button(
             self.text_frame,
-            text = "accept",
+            text = "paint",
             width=15,
             height = 3,
             bg="#668B8B",
             fg = "#F0F0F0",
-            command = self.set_gameboard
+            command = self.paint_gameboard
+        )
+
+        self.get_text_button = tk.Button(
+            self.text_frame,
+            text = "get text",
+            width=15,
+            height = 3,
+            bg="#668B8B",
+            fg = "#F0F0F0",
+            command = self.set_text
         )
 
         self.text_field.grid(columnspan=3, row = 0, column = 0, padx='5', pady='5')
         self.scroll_bar.grid(row = 0, column = 3, rowspan = 2, sticky = tk.N + tk.S+tk.W)
-        self.accept_button.grid(row = 0, column = 4, rowspan = 2)
+        self.paint_button.grid(row = 0, column = 4, rowspan = 1)
+        self.get_text_button.grid(row = 0, column = 5, rowspan = 1)
         self.sample_selector.grid(row = 0, column = 6 , padx='5', pady='5')
         
         self.text_frame.pack()
@@ -88,48 +167,37 @@ class GolpyGui():
         self.speed.grid(row = 0, column =2, padx='5', pady='5')
         self.expand_checkbox.grid(row = 0, column = 3, padx='5', pady='5')
         self.button_frame.pack()
+     
 
-        self.gameboard_label = tk.Text(self.window, height = 4, width = 50, font="Webdings" )
+        self.gameboard = gm.create_gameboard(20, 20)
+        self.canvas = DrawableGrid(self.window, self, self.gameboard, size=20)
+        self.canvas.pack(fill="both", expand=True)
+
 
         tk.mainloop()
     
     
-    def set_gameboard(self):
+    def paint_gameboard(self):
         gb_txt = self.text_field.get(1.0, tk.END)
         
-        self.gameboard_label.pack_forget()
-
         self.gameboard = sim.convert_to_gameboard(gb_txt)
-        font_size = self.get_font_size(self.gameboard)
-        self.gameboard_label = tk.Text(self.window, height = self.gameboard.shape[0], width = self.gameboard.shape[1], font = ("Webdings", self.get_font_size(self.gameboard)) )
         
-        #self.gameboard_label.configure(
-        #                    text = gm.get_gameboard_text_compact(self.gameboard),
-        #                    font=("Webdings", font_size))
-        
-        self.gameboard_label.delete(1.0, tk.END)
-        self.gameboard_label.insert(tk.END, gm.get_gameboard_text_compact(self.gameboard))
+        self.canvas.pack_forget()
+        self.canvas = DrawableGrid(self.window, self, self.gameboard, size = 20)
+        self.canvas.pack(fill="both", expand=True)
 
 
-        self.gameboard_label.config(spacing1=-1)    # Spacing above the first line in a block of text
-        self.gameboard_label.config(spacing2=-1)    # Spacing between the lines in a block of text
-        self.gameboard_label.config(spacing3=-1)    # Spacing after the lines of text
+    def set_text_to_value(self, text_str):
+        self.text_field.delete(1.0, tk.END)
+        self.text_field.insert(tk.END, text_str)
 
-
-
-        self.gameboard_label.pack_propagate(0)
-        self.gameboard_label.pack()
-
-    def get_font_size(self, gameboard):
-        return 8
-        max_dim = max(gameboard.shape)
-        if max_dim < 50:
-            return 12
-        font_size = 14 - max_dim // 15
-        return font_size
-
+    def set_text(self):
+        self.gameboard = self.canvas.get_gameboard()
+        gb_str = sim.convert_to_string(self.gameboard)
+        self.set_text_to_value(gb_str)
+   
     def nextCallBack(self):
-        
+        self.set_text_to_value("")
         gb = gm.play(self.gameboard)
         if gm.gameboard_equal(gb, self.gameboard):
             self.running = False
@@ -138,19 +206,17 @@ class GolpyGui():
         self.gameboard = gb
         if self.expand.get() == 1:
             self.gameboard = gam.expand_gameboard_if_necessary(self.gameboard)
-        gb_str = sim.convert_to_string(self.gameboard)        
-        self.text_field.delete(1.0, tk.END)
-        self.text_field.insert(tk.END, gb_str)
+        
+        if self.gameboard.shape != gb.shape:
+            
+            self.canvas.pack_forget()
+            self.canvas = DrawableGrid(self.window, self, self.gameboard, size = 20)
+            self.canvas.pack(fill="both", expand=True)
+
+        else:
+            self.canvas.set_gameboard(gb)
 
         
-        self.gameboard_label.delete(1.0, tk.END)
-        self.gameboard_label.configure(font = ("Webdings", self.get_font_size(self.gameboard)))
-        self.gameboard_label.configure(height = self.gameboard.shape[0])
-        self.gameboard_label.configure(width = self.gameboard.shape[1])
-        self.gameboard_label.insert(tk.END, gm.get_gameboard_text_compact(self.gameboard))
-        
-
-        self.gameboard_label.pack()
         
     def runCallBack(self):
         if self.running:
@@ -164,7 +230,10 @@ class GolpyGui():
     def play(self):
         if self.running:
             self.nextCallBack()
-            self.window.after((int)(10 / self.speed.get() * 100), self.play)
+            if self.speed.get() == 10:
+                self.window.after(10, self.play)
+            else:
+                self.window.after((int)(10 / self.speed.get() * 100), self.play)
 
 
     def selectSample(self, val):
@@ -172,8 +241,7 @@ class GolpyGui():
         #self.set_gameboard(val)
         gb = samples.sample_dict[val]
         gb_str = sim.convert_to_string(gb)        
-        self.text_field.delete(1.0, tk.END)
-        self.text_field.insert(tk.END, gb_str)
+        self.set_text_to_value(gb_str)
 
 def main():
     #gleiter = samples.get_gleiter(16)
